d2528f3c644f4d27a012a29d85011217d9635388
Sebastian <sebastoledo009@gmail.com>
2025-07-09 12:41:11 -0500

    upd: prediccion para un producto

--- inventia-app/src/main/java/com/inventia/inventia_app/controllers/PredictionController.java
+++ inventia-app/src/main/java/com/inventia/inventia_app/controllers/PredictionController.java
@@ -1,24 +1,43 @@
 package com.inventia.inventia_app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.inventia.inventia_app.entities.Product;
+import com.inventia.inventia_app.services.PredictionService;
+
+import reactor.core.publisher.Flux;
 
 /**
  * PredictionController
  */
 @RestController
+@RequestMapping("/predict")
 public class PredictionController {
 
+    private PredictionService predictionService;
+
+    @Autowired
+    public PredictionController(PredictionService predictionService) {
+        this.predictionService = predictionService;
+    }
+
     public PredictionController() {
+        //predictionService = new PredictionService(webClientBuilder);
     }
 
-    @GetMapping("/predict-uno")
-    public String predecirUnico(@RequestBody String body){
-        return "prediccion";
+    @PostMapping("/single")
+    public Flux<String> predecirUnico(@RequestBody Product product){
+        System.out.println(product.getProduct_id());
+        return predictionService.predictSingle(product);
     }
 
-    @GetMapping("/predict-grupo")
+    @PostMapping("/group")
     public String predecirGrupo(@RequestBody String body){
         return "prediccion";
     }
---
+++ inventia-app/src/main/java/com/inventia/inventia_app/entities/PredictionResponse.java
@@ -0,0 +1,51 @@
+package com.inventia.inventia_app.entities;
+
+/**
+ * PredictionResponse
+ */
+public class PredictionResponse {
+
+    private String status;
+    private Integer product_id;
+    private String fecha;
+    private Double prediccion;
+
+    public PredictionResponse(String status, Integer product_id, String fecha, Double prediccion){
+        this.status = status;
+        this.product_id = product_id;
+        this.fecha = fecha;
+        this.prediccion = prediccion;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setProductId(Integer product_id) {
+        this.product_id = product_id;
+    }
+
+    public Integer getProductId() {
+        return this.product_id;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getFecha() {
+        return this.fecha;
+    }
+
+    public void setPrediccion(Double prediccion) {
+        this.prediccion = prediccion;
+    }
+
+    public Double getPrediccion() {
+        return this.prediccion;
+    }
+}
